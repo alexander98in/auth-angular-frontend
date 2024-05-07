a843db6f71670b0919064fe50210457658b8ccc9
xander98in <leyderin@unicauca.edu.co>
2024-05-07 08:39:43 -0500

    Creacion de interfaces y servicio

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrl: './login-page.component.css'
 })
 export class LoginPageComponent {
+
+  private fb = inject(FormBuilder);
+
+  public myForm: FormGroup = this.fb.group({
+    email: ['alex@gmail.com', [Validators.required, Validators.email]],
+    password: ['123456', [Validators.required, Validators.minLength(6)]]
+  });
+
+  login(): void {
+    console.log(this.myForm.value);
+    
+  }
 
 }
